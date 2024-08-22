da3908870db23065fbf96c1d3d088900a544615a
Ahmet Burak Arslan <arslanahmet4772@gmail.com>
2024-08-22 16:04:44 +0200

    config input edited

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,22 @@ import { Input } from "./components/ui/input"
 
 function App() {
   const [type, setType] = useState<string>("")
-  const [value, setValue] = useState<any>(10)
+  const [Time, setTime] = useState<number>(60)
+  const [NumberOfQuetions, setNumberOfQuetions] = useState<any>(10)
 
-  const setNumberOfQuetions = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const settime = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valueConfig = /^(\d*)$/
+    const newValue = e.target.value
+    if (valueConfig.test(newValue)) {
+      newValue === "" ? setTime(0) : setTime(Number(newValue))
+    }
+  }
+
+  const setNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     const valueConfig = /^([0-9]|[1-4][0-9]|50)?$/
     const newValue = e.target.value
     if (valueConfig.test(newValue)) {
-      setValue(newValue)
+      newValue === "" ? setNumberOfQuetions(0) : setNumberOfQuetions(Number(newValue))
     }
   }
 
@@ -24,16 +33,16 @@ function App() {
           <h1 className="text-white font-bold w-full text-left">Settings</h1>
         </CardHeader>
         <CardContent>
-          <div className="flex flex-row gap-1">
+          <div className="flex flex-row">
             {/* <SelectNumberOfQuestions /> */}
-            <Input onChange={setNumberOfQuetions} value={value} className="bg-zinc-700 border-none text-white my-1" placeholder="Number Of Questions max-50" />
+            <Input onChange={setNumber} value={NumberOfQuetions} className="bg-zinc-700 border-none text-white mr-0.5" placeholder="Number Of Questions max-50" />
             {/* <SelectTime /> */}
-            <Input className="bg-zinc-700 border-none text-white my-1" placeholder="Time" />
+            <Input onChange={settime} value={Time} className="bg-zinc-700 border-none text-white ml-0.5" placeholder="Time (second)" />
           </div>
           {/* <SelectType /> */}
           <Select>
             <SelectTrigger className="bg-zinc-700 border-none text-white my-1">
-              <SelectValue onChange={(e: React.ChangeEvent<HTMLSelectElement>) => { setType(e.target.value); console.log(type) }} placeholder="Select Type" />
+              <SelectValue placeholder="Select Type" />
             </SelectTrigger>
             <SelectContent className="bg-zinc-700 border-none">
               <SelectItem className="text-white" value="any">Any</SelectItem>
@@ -48,30 +57,42 @@ function App() {
             </SelectTrigger>
             <SelectContent className="bg-zinc-700 border-none">
               <SelectItem className="text-white" value="any">Any Category</SelectItem>
-              <SelectItem className="text-white" value="multiple">General Knowledge</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Books</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Film</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Music</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Musicals & Theatres</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Television</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Video Games</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Board Games</SelectItem>
-              <SelectItem className="text-white" value="boolean">Science & Nature</SelectItem>
-              <SelectItem className="text-white" value="boolean">Science: Computers</SelectItem>
-              <SelectItem className="text-white" value="boolean">Science: Mathematics</SelectItem>
-              <SelectItem className="text-white" value="boolean">Mythology</SelectItem>
-              <SelectItem className="text-white" value="boolean">Sports</SelectItem>
-              <SelectItem className="text-white" value="boolean">Geography</SelectItem>
-              <SelectItem className="text-white" value="boolean">History</SelectItem>
-              <SelectItem className="text-white" value="boolean">Politics</SelectItem>
-              <SelectItem className="text-white" value="boolean">Art</SelectItem>
-              <SelectItem className="text-white" value="boolean">Celebrities</SelectItem>
-              <SelectItem className="text-white" value="boolean">Animals</SelectItem>
-              <SelectItem className="text-white" value="boolean">Vehicles</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Comics</SelectItem>
-              <SelectItem className="text-white" value="boolean">Science: Gadgets</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Japanese Anime & Manga</SelectItem>
-              <SelectItem className="text-white" value="boolean">Entertainment: Cartoon & Animations</SelectItem>
+              <SelectItem className="text-white" value="9">General Knowledge</SelectItem>
+              <SelectItem className="text-white" value="10">Entertainment: Books</SelectItem>
+              <SelectItem className="text-white" value="11">Entertainment: Film</SelectItem>
+              <SelectItem className="text-white" value="12">Entertainment: Music</SelectItem>
+              <SelectItem className="text-white" value="13">Entertainment: Musicals & Theatres</SelectItem>
+              <SelectItem className="text-white" value="14">Entertainment: Television</SelectItem>
+              <SelectItem className="text-white" value="15">Entertainment: Video Games</SelectItem>
+              <SelectItem className="text-white" value="16">Entertainment: Board Games</SelectItem>
+              <SelectItem className="text-white" value="17">Science & Nature</SelectItem>
+              <SelectItem className="text-white" value="18">Science: Computers</SelectItem>
+              <SelectItem className="text-white" value="19">Science: Mathematics</SelectItem>
+              <SelectItem className="text-white" value="20">Mythology</SelectItem>
+              <SelectItem className="text-white" value="21">Sports</SelectItem>
+              <SelectItem className="text-white" value="22">Geography</SelectItem>
+              <SelectItem className="text-white" value="23">History</SelectItem>
+              <SelectItem className="text-white" value="24">Politics</SelectItem>
+              <SelectItem className="text-white" value="25">Art</SelectItem>
+              <SelectItem className="text-white" value="26">Celebrities</SelectItem>
+              <SelectItem className="text-white" value="27">Animals</SelectItem>
+              <SelectItem className="text-white" value="28">Vehicles</SelectItem>
+              <SelectItem className="text-white" value="29">Entertainment: Comics</SelectItem>
+              <SelectItem className="text-white" value="30">Science: Gadgets</SelectItem>
+              <SelectItem className="text-white" value="31">Entertainment: Japanese Anime & Manga</SelectItem>
+              <SelectItem className="text-white" value="32">Entertainment: Cartoon & Animations</SelectItem>
+            </SelectContent>
+          </Select>
+          {/* <SelectDifficulty /> */}
+          <Select>
+            <SelectTrigger className="bg-zinc-700 border-none text-white my-1">
+              <SelectValue placeholder="Select Difficulty" />
+            </SelectTrigger>
+            <SelectContent className="bg-zinc-700 border-none">
+              <SelectItem className="text-white" value="any">Any Difficulty</SelectItem>
+              <SelectItem className="text-white" value="easy">Easy</SelectItem>
+              <SelectItem className="text-white" value="medium">Medium</SelectItem>
+              <SelectItem className="text-white" value="hard">Hard</SelectItem>
             </SelectContent>
           </Select>
         </CardContent>
